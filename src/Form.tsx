213b5fac23609e2d4a29161d2de3a41da4e78ff5
Sofia Noor Rafa <sofiautilitarian@gmail.com>
2025-04-14 01:13:31 +0600

    Updating React Forms from my local project space

---
+++ src/Form.tsx
@@ -0,0 +1,40 @@
+import React, { FormEvent, useRef, useState } from "react";
+
+const Form = () => {
+  const address = useRef<HTMLInputElement>(null);
+  const [IP_Value, setIP_Value] = useState({
+    ip: "",
+  });
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (address.current !== null) IP_Value.ip = address.current.value;
+    console.log(IP_Value.ip);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          IP Address
+        </label>
+
+        <input
+          onChange={(event) =>
+            setIP_Value({ ...IP_Value, ip: event.target.value })
+          }
+          value = {IP_Value.ip}
+          id="name"
+          ref={address}
+          type="text"
+          pattern="^(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})){3}$"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/moduleFive.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { FormEvent } from "react";
+
+const moduleFive = () => {
+  useState({
+    
+  });
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
+  return <div>moduleFive</div>;
+};
+
+export default moduleFive;
